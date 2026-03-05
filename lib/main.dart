6d9bcc7f115e3a934100f8a168609999438a1a2b
Isabella751 <isabella.leite562@gmail.com>
2026-03-05 11:04:14 -0300

    Segunda tela em andamento

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:new_project/widgets/titulo_sessao.dart';
+import 'package:new_project/screens/widgets_conteudo.dart';
+import 'package:new_project/screens/widgets_layout.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,91 +14,30 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
-      home: Scaffold(
-        appBar: AppBar(title: Text('Widget de  conteúdo')),
-        body: ListView(
-          padding: EdgeInsets.all(25),
-          children: [
-            TituloSessao(titulo: 'Texto de exemplo'),
-            Text(
-              'Texto de conteudo ',
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-                color: Colors.deepPurple,
-              ),
-            ),
-
-            Divider(),
-            TituloSessao(titulo: 'Imagem de exemplo'),
-            Image.network('https://picsum.photos/id/237/200/300'),
-
-            Divider(),
-            TituloSessao(titulo: 'Icone'),
-            Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(Icons.favorite, color: Colors.red, size: 50,),
-              Icon(Icons.star, color: Colors.yellow, size: 50,),
-              Icon(Icons.settings, color: Colors.red, size: 50,),
-            ],),
-
-            Divider(),
-            TituloSessao(titulo: 'Elevated Button'),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text('Pressione'),
-            ),
-
-          ],
-        ),
-      ),
-    );
+      home: ListContents(),
+      );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class ListContents extends StatelessWidget {
+  const ListContents({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: .center,
+        appBar: AppBar(title: Text('Catálogo de Widgets'),),
+        body: Column(
           children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            ElevatedButton(onPressed: () => Navigator.push(context, 
+            MaterialPageRoute(builder: (_) => WidgetsConteudo())
+            ), 
+            child: Text('Widgets de conteúdo'),),
+            ElevatedButton(onPressed: () => Navigator.push(context, 
+            MaterialPageRoute(builder: (_) => WidgetsLayout())
+            ), 
+            child: Text('Widgets de Layout'),),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
+      );
   }
 }
--- lib/screens/widgets_conteudo.dart
+++ lib/screens/widgets_conteudo.dart
@@ -1,10 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:new_project/widgets/titulo_sessao.dart';
 
 class WidgetsConteudo extends StatelessWidget {
   const WidgetsConteudo({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        appBar: AppBar(title: Text('Widget de  conteúdo')),
+        body: ListView(
+          padding: EdgeInsets.all(25),
+          children: [
+            TituloSessao(titulo: 'Texto de exemplo'),
+            Text(
+              'Texto de conteudo ',
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Colors.deepPurple,
+              ),
+            ),
+
+            Divider(),
+            TituloSessao(titulo: 'Imagem de exemplo'),
+            Image.network('https://picsum.photos/id/237/200/300'),
+
+            Divider(),
+            TituloSessao(titulo: 'Icone'),
+            Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.favorite, color: Colors.red, size: 50,),
+              Icon(Icons.star, color: Colors.yellow, size: 50,),
+              Icon(Icons.settings, color: Colors.red, size: 50,),
+            ],),
+
+            Divider(),
+            TituloSessao(titulo: 'Elevated Button'),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('Pressione'),
+            ),
+
+          ],
+        ),
+      );
   }
 }
